826ea015c2589107d8b075d3e3e1843a45b500f6
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2021-02-08 17:28:29 +0900

    Create pushpush.cpp

---
+++ pushpush.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Consola.h"
+#include "start_menu.h"
+using namespace std;
+
+int main()
+{
+	clrscr();
+	_setcursortype(_NOCURSOR);
+	map::_start_menu();
+	return 0;
+}
