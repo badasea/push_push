a7fdd2246bc190fb2798576eaca031b6668a09bb
jungbada269 <57929751+jungbada269@users.noreply.github.com>
2021-02-08 17:27:10 +0900

    Create keycode.h

---
+++ keycode.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// 이동키
+#define UPKEY 0xe048
+#define LEFTKEY 0xe04b
+#define RIGHTKEY 0xe04d
+#define DOWNKEY 0xe050
+// 선택버튼
+#define SPACE 0x20
+// 리셋
+#define RKEY 0x72
+// 이전 스테이지
+#define AKEY 0x61
+// 다음 스테이지
+#define SKEY 0x73
+// 한수 무르기
+#define ZKEY 0x7A
+// 음악 ON OFF
+#define PKEY 0x70
+// 게임 포기
+#define GKEY 0x67
